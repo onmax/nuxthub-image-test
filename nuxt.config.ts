ddd8cc6217104a82440e9881fcd85c8479cb8c89
onmax <maximogarciamtnez@gmail.com>
2025-12-12 11:14:36 +0100

    vercel: nuxthub image provider demo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxthub/core', '@nuxt/image'],
-  hub: { blob: true },
-  image: { provider: 'nuxthub' },
+  hub: { blob: { image: { path: '/images' } } },
+  nitro: { preset: 'vercel' },
   devtools: { enabled: true }
 })
